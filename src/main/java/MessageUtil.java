a2af60e802a5692dd36896649f9edf63759e6a35
dsreekant211 <40782606+dsreekant211@users.noreply.github.com>
2019-03-19 11:10:47 +0530

    Update MessageUtil.java

--- src/main/java/MessageUtil.java
+++ src/main/java/MessageUtil.java
@@ -11,7 +11,7 @@ public class MessageUtil {
   }
 
   public String salutationMessage() {
-    message = "hello!" + message;
+    message = "Hi!" + message;
     System.out.println(message);
     return message;
   }
